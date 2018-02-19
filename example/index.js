1d5585159896b2f4d19f40e2bcd8126a0b644992
Luke Burns <lukemburns@gmail.com>
2018-02-19 17:14:18 -0500

    tidy example

--- example/index.js
+++ example/index.js
@@ -5,12 +5,15 @@ _.log = true
 // create observable state
 const state = _({ player: 1, time: 0 })
 
+state.nextPlayer = () => state.player++
+state.tick = () => state.time++
+
 // compose reactive views 
 const timer = _(state => html`<div>
-  ${state.time}
+  Time: ${state.time}
 </div>`)
 
-const body = _(state => html`<body onclick=${() => state.player++}>
+const body = _(state => html`<body onclick=${state.nextPlayer}>
   <h1>Player ${state.player}</h1>
   ${timer(state)}
 </body>`)
@@ -19,4 +22,4 @@ const body = _(state => html`<body onclick=${() => state.player++}>
 body(state, document.body)
 
 // mutate state
-setInterval(() => state.time++, 200)
+setInterval(state.tick, 1)
