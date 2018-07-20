5db9ef8dfb996a01e78cc9360b3a34801b080c0f
Luke Burns <lukemburns@gmail.com>
2018-07-20 17:25:02 -0300

    proxy indexed cache

--- example/index.js
+++ example/index.js
@@ -1,23 +1,28 @@
 const html = require('bel')
 const _ = require('../')
 
-const state = _({ list: [] })
+const state = _({ items: [] })
 
-const list = _(state => html`<ul>
-  ${state.list.map(num => html`<li>${num}</li>`)}
+for (var i = 0; i < [0,1,2].length; i++) {
+  state.items.push({ num: Math.random() })
+}
+
+window.state = state
+
+const li = _(item => html`<li>${item.num}</li>`)
+const ul = _(state => html`<ul>
+  ${state.items.map(li)}
 </ul>`)
-
 const body = _(state => html`<body>
-  <button onclick=${() => state.list.push(Math.random())}>Append random number</button>
-  ${list(state)}
+  ${ul(state)}
 </body>`)
 
 document.body = body(state)
 
 
-list.on('load', () => console.log('loaded list'))
-list.on('morph', () => console.log('morphed list'))
-list.on('unload', () => console.log('unloaded list'))
+ul.on('load', () => console.log('loaded list'))
+ul.on('morph', () => console.log('morphed list'))
+ul.on('unload', () => console.log('unloaded list'))
 
 body.on('load', () => console.log('loaded body'))
 body.on('morph', () => console.log('morphed body'))
--- index.js
+++ index.js
@@ -8,28 +8,36 @@ let i = 1
 
 function morphable (view) {
   if (typeof view !== 'function') return observable(view)
-  
+
   const id = i++
   let reaction
-  let cached
   let self = this
-  
+  let cache = new WeakMap()
+
   const fn = function () {
     let args = Array.from(arguments)
     let rawArgs = args.map(state => morphable.raw(state))
-    let element = cached || view.apply(self, rawArgs)
+    let index = args[0]
+
+    let element
+    console.log(args[0])
+    if (cache.has(index)) {
+      element = cache.get(index)
+    } else {
+      element = view.apply(self, rawArgs)
+    }
     element.id = element.id || id
-    
+
     return onload(element, function (el) {
       if (reaction) return
-      cached = el
-      
+      cache.set(index, el)
+
       let init = false
       reaction = observe(() => {
         fn.emit('premorph', morphable.raw(self), el, ...rawArgs)
         let update = view.apply(self, args)
         update.id = update.id || id
-        update.setAttribute(KEY_ATTR, cached.getAttribute(KEY_ATTR))
+        update.setAttribute(KEY_ATTR, cache.get(index).getAttribute(KEY_ATTR))
         morph(el, update)
         if (init) {
           fn.emit('morph', morphable.raw(self), el, ...rawArgs)
