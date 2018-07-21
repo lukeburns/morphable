b6a811eb739bedc5181c272a881cca343f8409b0
Luke Burns <lukemburns@gmail.com>
2018-07-20 21:06:25 -0300

    multielement views using weakmaps indexed by proxies

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ const onload = require('on-load')
 const morph = require('nanomorph')
 const util = require('@nx-js/observer-util')
 const events = require('events')
-const { observable, observe, unobserve } = util
+const { observable, observe, unobserve, isObservable } = util
 const KEY_ATTR = 'data-' + onload.KEY_ID
-let i = 1
+let id = 1
 
 function morphable (view) {
   if (typeof view !== 'function') return observable(view)
 
-  const id = i++
-  let reaction
   let self = this
   let cache = new WeakMap()
+  let reactions = new WeakMap()
+  let int = 1
 
   const fn = function () {
     let args = Array.from(arguments)
@@ -20,24 +20,26 @@ function morphable (view) {
     let index = args[0]
 
     let element
-    console.log(args[0])
     if (cache.has(index)) {
       element = cache.get(index)
     } else {
       element = view.apply(self, rawArgs)
+      element.id = id++
     }
-    element.id = element.id || id
 
     return onload(element, function (el) {
-      if (reaction) return
-      cache.set(index, el)
+      if (isObservable(index)) {
+        if (reactions.has(index)) return
+        cache.set(index, el)
+      }
 
       let init = false
-      reaction = observe(() => {
+      let int = el.id
+      let reaction = observe(() => {
         fn.emit('premorph', morphable.raw(self), el, ...rawArgs)
         let update = view.apply(self, args)
-        update.id = update.id || id
-        update.setAttribute(KEY_ATTR, cache.get(index).getAttribute(KEY_ATTR))
+        update.id = update.id || int
+        update.setAttribute(KEY_ATTR, (cache.get(index) || el).getAttribute(KEY_ATTR))
         morph(el, update)
         if (init) {
           fn.emit('morph', morphable.raw(self), el, ...rawArgs)
@@ -46,11 +48,13 @@ function morphable (view) {
           init = true
         }
       })
+
+      if (isObservable(index)) reactions.set(index, reaction)
     }, el => {
-      if (!reaction) return
+      if (!reactions.has(index)) return
       fn.emit('unload', morphable.raw(self), el, ...rawArgs)
-      unobserve(reaction)
-      reaction = null
+      unobserve(reactions.get(index))
+      reactions.delete(index)
     }, id)
   }
   events.call(fn)
